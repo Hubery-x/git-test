76609de2a2e4ed98f87c935e5dc5aed48bc3b3ac
Maple <849648796@qq.com>
2022-06-01 20:51:54 +0800

    commit pushTest

--- src/main/java/com/zqu/git/git.java
+++ src/main/java/com/zqu/git/git.java
@@ -7,5 +7,6 @@ public class git {
         System.out.println("git测试hot-fix");
         System.out.println("冲突测试 master");
         System.out.println("冲突测试 hot-fix");
+        System.out.println("push");
     }
 }
